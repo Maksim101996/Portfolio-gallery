2d66da737a0108e96da7ec66f2195aa00c8d1709
Maksim101996 <akakii.botov@mail.com>
2023-04-15 14:40:25 +0500

    fixed error src

--- src/App.tsx
+++ src/App.tsx
@@ -7,44 +7,44 @@ function App() {
 			<MainGallery photos={[
 				{
 					id: 1,
-					src: "public/photos/1.jpg",
-					preview: "public/photos/preview/1.jpg",
+					src: "/photos/1.jpg",
+					preview: "/photos/preview/1.jpg",
 					desc: "nature1"
 				},
 				{
 					id: 2,
-					src: "public/photos/2.jpg",
-					preview: "public/photos/preview/2.jpg",
+					src: "/photos/2.jpg",
+					preview: "/photos/preview/2.jpg",
 					desc: "nature2"
 				},
 				{
 					id: 3,
-					src: "public/photos/3.jpg",
-					preview: "public/photos/preview/3.jpg",
+					src: "/photos/3.jpg",
+					preview: "/photos/preview/3.jpg",
 					desc: "nature3"
 				},
 				{
 					id: 4,
-					src: "public/photos/4.jpg",
-					preview: "public/photos/preview/4.jpg",
+					src: "/photos/4.jpg",
+					preview: "/photos/preview/4.jpg",
 					desc: "nature4"
 				},
 				{
 					id: 5,
-					src: "public/photos/5.jpg",
-					preview: "public/photos/preview/5.jpg",
+					src: "/photos/5.jpg",
+					preview: "/photos/preview/5.jpg",
 					desc: "nature5"
 				},
 				{
 					id: 6,
-					src: "public/photos/6.jpg",
-					preview: "public/photos/preview/6.jpg",
+					src: "/photos/6.jpg",
+					preview: "/photos/preview/6.jpg",
 					desc: "nature6"
 				},
 				{
 					id: 7,
-					src: "public/photos/7.jpg",
-					preview: "public/photos/preview/7.jpg",
+					src: "/photos/7.jpg",
+					preview: "/photos/preview/7.jpg",
 					desc: "nature7"
 				},
 
