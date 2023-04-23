e16c5377c40ebd98cc77487ca6f32171d98b58f7
Maksim101996 <akakii.botov@mail.com>
2023-04-23 21:30:43 +0500

    refactoring photos

--- src/App.tsx
+++ src/App.tsx
@@ -7,38 +7,38 @@ function App() {
 			<MainGallery photos={[
 				{
 					id: 1,
-					src: "/photos/screen/1.png",
-					preview: "/photos/screen/1.png",
+					src: "/photos/screen/1.jpg",
+					preview: "/photos/preview/1.jpg",
 					desc: "nature1"
 				},
 				{
 					id: 2,
-					src: "/photos/screen/2.png",
-					preview: "/photos/screen/2.png",
+					src: "/photos/screen/2.jpg",
+					preview: "/photos/preview/2.jpg",
 					desc: "nature2"
 				},
 				{
 					id: 3,
-					src: "/photos/screen/3.png",
-					preview: "/photos/screen/3.png",
+					src: "/photos/screen/3.jpg",
+					preview: "/photos/preview/3.jpg",
 					desc: "nature3"
 				},
 				{
 					id: 4,
-					src: "/photos/screen/4.png",
-					preview: "/photos/screen/4.png",
+					src: "/photos/screen/4.jpg",
+					preview: "/photos/preview/4.jpg",
 					desc: "nature4"
 				},
 				{
 					id: 5,
-					src: "/photos/screen/5.png",
-					preview: "/photos/screen/5.png",
+					src: "/photos/screen/5.jpg",
+					preview: "/photos/preview/5.jpg",
 					desc: "nature5"
 				},
 				{
 					id: 6,
-					src: "/photos/screen/6.png",
-					preview: "/photos/screen/6.png",
+					src: "/photos/screen/6.jpg",
+					preview: "/photos/preview/6.jpg",
 					desc: "nature6"
 				},
 			]} />
